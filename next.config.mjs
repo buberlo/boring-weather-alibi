87c9aa343f70e065b1e4c3e459b330387889f849
Appshaker <appshaker@localhost>
2026-08-25 16:35:59 +0200

    feat: add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['better-sqlite3'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'better-sqlite3': 'commonjs better-sqlite3',
+      });
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
